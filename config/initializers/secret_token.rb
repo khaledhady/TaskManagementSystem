c570dc73a402e8713fc2a258ca24806eebfce15d
Ahmed Al Abbassy <ahmed.alabbassy@badrit.com>
2012-09-09 07:12:44 +0200

    Just Created Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskManagementSystem::Application.config.secret_token = '1e5c22260bcbfbb627dd7b6b7365d9f77b4dfb1a16846da6354a76335b9483cb80e5d9e84a31d080a313f06a3e9413f132d7f6b25ec4f657c93a98d788a3ea09'
